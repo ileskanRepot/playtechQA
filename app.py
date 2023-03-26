6ed0be3a46bd2e018615c8b77366d033a998c11b
Ileska <ileska@tutanota.com>
2023-03-26 18:09:16 +0300

    It goes to inters page

--- app.py
+++ app.py
@@ -11,50 +11,30 @@ from time import sleep
 # If set to True it does as bonus task descripted
 useCoords = False
 
-timeout = 15
+textToSearch = "get a personal mentor, who will help and support you during the internship period"
+
+timeout = 5
 
 with webdriver.Chrome() as driver:
-    
+
+
     driver.get("https://playtech.ee")
-    sleep(5)
-    driver.find_element(By.CLASS_NAME, "custom-btn.text-nowrap.is-simple.ml-xl-2.mr-3.mr-xl-0.mb-2").click()
-    internsshipE = WebDriverWait(driver, timeout).until(
+    WebDriverWait(driver, timeout).until(
         EC.presence_of_element_located((
-            By.XPATH, '//a[@href="/internship"]')))
+            By.CLASS_NAME, "custom-btn.text-nowrap.is-simple.ml-xl-2.mr-3.mr-xl-0.mb-2"))).click()
+    internsshipEList = WebDriverWait(driver, timeout).until(
+        EC.presence_of_all_elements_located((
+            By.CLASS_NAME, "d-block.font-size-sm.font-weight-bold.text-uppercase.styled-link.mb-1.mb-xl-0.mr-sm-3.mr-xl-4")))
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    for o in internsshipEList:
+        if o.text == "INTERNSHIP":
+            internsshipE = o
 
-
-    sleep(timeout)
-    loc = internsshipE.location_once_scrolled_into_view
-    siz = internsshipE.size
-    wMin, hMin = loc["y"], loc["x"]
-
-    wMax = wMin + siz["width"]
-    hMax = hMin + siz["height"]
-    print("loc",loc)
-    print("siz",siz)
-    print(hMax,wMax)
-
-    crash()
-    sleep(100)
-    if useCoords:
-        ac = ActionChains(driver)
-        ac.move_by_offset(wMax, hMax)
-        ac.click()
-        ac.perform()
-        
-        driver.execute_script(f"window.scrollTo(0, {hMax});")
-    else:
-        # print(internsshipE.tag_name)
-        # print(hMax)
-        driver.refresh()
-
-        internsshipE.click()
-
-
-    print(loc)
-    sleep(100)
-
-    # print(driver.get_window_size()) # GET WINDOW SIZE
-    # first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3")))
-    # print(first_result.get_attribute("textContent"))
+    internsshipE.click()
+    try:
+        WebDriverWait(driver, timeout).until(
+            EC.presence_of_all_elements_located((
+                By.XPATH,"//*[contains(text(),\""+textToSearch+"\")]")))
+    except TimeoutExpection:
+        print("TEXT:",textToSearch,"Was not in page")
+sleep(100)
